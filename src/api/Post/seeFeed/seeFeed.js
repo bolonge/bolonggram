73d13f22266497b97e387f57eecc8f673fb85afb
bolonge <xxs456@naver.com>
2019-06-04 09:54:58 +0900

    update

--- src/api/Post/seeFeed/seeFeed.js
+++ src/api/Post/seeFeed/seeFeed.js
@@ -12,7 +12,7 @@ export default {
             id_in: [...following.map(user => user.id), user.id]
           }
         },
-        orderBy: "caption_ASC"
+        orderBy: "caption_DESC"
       });
     }
   }
